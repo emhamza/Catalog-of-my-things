79bed9f3f78b666d45ccb6c585604a53a3e9a501
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-04 13:42:52 +0300

    Update the id

--- book.rb
+++ book.rb
@@ -1,8 +1,13 @@
 class Book < Item
     attr_accessor :publisher, :cover_state
-    def initialize(id, publish_date, archived, label, publisher, cover_state )
-    super(id, publish_date, archived, label)
-    @publisher = publisher
-    @cover_state = cover_state
+  
+    def initialize(id = rand, publish_date, archived, label, publisher, cover_state)
+      super(id, publish_date, archived, label)
+      @publisher = publisher
+      @cover_state = cover_state
     end
-end
+  
+    def can_be_archived?
+    end
+  end
+  
