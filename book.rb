b029dc8c28e5d1ef5d4466b980e0edb1f8f39433
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-04 14:41:30 +0300

    Implement can be archieved method in class Book

--- book.rb
+++ book.rb
@@ -1,9 +1,16 @@
-class Book < Item
-  attr_accessor :publisher, :cover_state
+require_relative 'item'
 
-  def initialize(publisher, cover_state, publish_date: nil, archived: false)
-    super(publish_date: publish_date, archived: archived)
-    @publisher = publisher
-    @cover_state = cover_state
+class Book < Item
+    attr_accessor :publisher, :cover_state
+  
+    def initialize(publisher, cover_state, publish_date: nil, archived: false)
+      super(publish_date: publish_date, archived: archived)
+      @publisher = publisher
+      @cover_state = cover_state
+    end
+  
+    def can_be_archived?
+      super || @cover_state == "bad"
+    end
   end
-end
+  
