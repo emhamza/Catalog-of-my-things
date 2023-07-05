c11c9f2ce7d0f7356a573ff65afcf87f187515e5
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-07-05 21:23:08 +0300

    Update with book.json file to save data

--- book.rb
+++ book.rb
@@ -1,16 +1,44 @@
-require_relative 'item'
+require_relative 'label'
+require 'json'
 
-class Book < Item
-  attr_accessor :publisher, :cover_state
+class Book
+  attr_accessor :publisher, :cover_state, :publish_date
+  attr_reader :id, :title, :label
 
-  def initialize(publish_date, publisher, cover_state: String)
-    super(publish_date)
-    @publish_date = publish_date
+  def initialize(title, publisher, cover_state: 'good', publish_date: nil)
+    @id = rand(0...1000)
+    @title = title
     @publisher = publisher
     @cover_state = cover_state
+    @publish_date = publish_date
+    @label = nil
+  end
+
+  def label=(label)
+    @label = label
   end
 
   def can_be_archived?
-    super || @cover_state == 'bad'
+    @cover_state == 'bad'
+  end
+
+  def to_json
+    {
+      id: @id,
+      title: @title,
+      publisher: @publisher,
+      cover_state: @cover_state,
+      publish_date: @publish_date
+    }.to_json
+  end
+
+  def self.from_json(json_data)
+    data = JSON.parse(json_data)
+    new(
+      data['title'],
+      data['publisher'],
+      cover_state: data['cover_state'],
+      publish_date: data['publish_date']
+    )
   end
 end
--- met/manage_book.rb
+++ met/manage_book.rb
@@ -11,12 +11,29 @@ class BookManager
   end
 
   def display_options
-    puts ''
-    puts 'Welcome to my catalog of books! Please select an option by entering a number:'
-    puts '1. Add book'
-    puts '2. List all books'
-    puts '3. List labels'
-    puts '4. Exit'
+    loop do
+      puts ''
+      puts 'Welcome to my catalog of books! Please select an option by entering a number:'
+      puts '1. Add book'
+      puts '2. List all books'
+      puts '3. List labels'
+      puts '4. Exit'
+
+      choice = gets.chomp.to_i
+
+      case choice
+      when 1
+        add_book
+      when 2
+        list_all_books
+      when 3
+        list_labels
+      when 4
+        break
+      else
+        puts 'Invalid choice. Please try again.'
+      end
+    end
   end
 
   def add_book
@@ -36,7 +53,7 @@ class BookManager
     puts 'Enter publish date (YYYY-MM-DD): '
     publish_date = gets.chomp
 
-    book = Book.new(publisher, cover_state, publish_date: publish_date)
+    book = Book.new(title, publisher, cover_state: cover_state, publish_date: publish_date)
     label = Label.new(title, cover_state)
     label.add_item(book)
     book.label = label
@@ -54,11 +71,10 @@ class BookManager
         puts "Book title: #{book.title}, publisher: #{book.publisher}, cover state: #{book.cover_state}, publish date: #{book.publish_date}"
       end
     end
-    load_data_from_json
   end
 
   def list_labels
-    labels = @books.map(&:label).uniq
+    labels = @books.map(&:label).compact.uniq
     if labels.empty?
       puts 'You have no labels in your catalog.'
     else
@@ -66,25 +82,33 @@ class BookManager
         puts "Label title: #{label.title}, color: #{label.color}"
       end
     end
-    load_data_from_json
   end
+  
 
   private
 
   def save_data_to_json
-    File.write('books.json', JSON.pretty_generate(@books.map(&:to_json)))
+    File.write('./data/books.json', JSON.pretty_generate(@books.map(&:to_json)))
   end
 
   def load_data_from_json
-    if File.exist?('books.json')
-      books_file = File.read('books.json')
-      books_data = JSON.parse(books_file)
-      @books.clear
-      books_data.each do |book|
-        @books << Book.from_json(book)
+    if File.exist?('./data/books.json')
+      books_file = File.read('./data/books.json')
+      unless books_file.empty?
+        books_data = JSON.parse(books_file)
+        @books.clear
+        books_data.each do |book|
+          @books << Book.from_json(book)
+        end
+      else
+        puts 'Book data file is empty.'
       end
     else
       puts 'No book data file found.'
     end
   end
+  
 end
+
+manager = BookManager.new
+manager.display_options
