33c13ffdd3b1bbf1c5b78c61d37617272acd7a1a
mnaibei <muchajulius@gmail.com>
2023-07-05 11:52:45 +0300

    Added music_manager functions, updated music_album, added tests

--- met/manage_music_album.rb
+++ met/manage_music_album.rb
@@ -1,3 +1,69 @@
-def manage_music_album
-  puts 'manage music album goes here'
+require_relative '../genre'
+require_relative '../music_album'
+
+class MusicManager
+  attr_accessor :albums
+
+  def initialize
+    @albums = []
+  end
+
+  def display_options
+    puts ''
+    puts 'Welcome to my catalog of music albums! Please select an option by entering a number:'
+    puts '1. Add music album'
+    puts '2. List all music albums'
+    puts '3. List genres'
+    puts '4. Exit'
+  end
+
+  def add_music_album
+    puts ''
+    puts 'Enter album title: '
+    title = gets.chomp
+
+    puts ''
+    puts 'Is it on Spotify? (y/n)'
+    on_spotify = gets.chomp.downcase == 'y'
+
+    puts ''
+    puts 'Enter publish date (YYYY-MM-DD): '
+    publish_date = gets.chomp
+
+    puts ''
+    puts 'Enter genre: '
+    genre_name = gets.chomp.downcase
+
+    album = MusicAlbum.new(title, genre_name, on_spotify: on_spotify, publish_date: publish_date)
+    genre = Genre.new(genre_name)
+    genre.add_item(album)
+    album.genre = genre
+    @albums << album
+
+    puts "Added #{album.name} to your catalog."
+  end
+
+  def list_all_music_albums
+    if @albums.empty?
+      puts 'You have no music albums in your catalog.'
+    else
+      @albums.each do |album|
+        puts "Album title: #{album.name}, on Spotify: #{album.on_spotify}, publish date: #{album.publish_date}"
+      end
+    end
+  end
+
+  def list_genres
+    genres = @albums.map(&:genre).uniq
+    if genres.empty?
+      puts 'You have no genres in your catalog.'
+    else
+      genres.each do |genre|
+        puts "Genre: #{genre.name}"
+        genre.items.each do |item|
+          puts "Album title: #{item.name}, on Spotify: #{item.on_spotify}, publish date: #{item.publish_date}"
+        end
+      end
+    end
+  end
 end
--- music_album.rb
+++ music_album.rb
@@ -1,15 +1,36 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-  attr_accessor :name, :on_spotify
+  attr_accessor :name, :on_spotify, :genre
 
-  def initialize(name, on_spotify: false, publish_date: nil, archived: false)
+  def initialize(name, genre, on_spotify: false, publish_date: nil, archived: false)
     super(publish_date: publish_date, archived: archived)
     @name = name
     @on_spotify = on_spotify
+    @genre = genre
   end
 
   def can_be_archived?
     super && @on_spotify
+  end
+
+  def to_hash
+    {
+      name: @name,
+      genre_name: @genre,
+      on_spotify: @on_spotify,
+      publish_date: @publish_date,
+      archived: @archived
+    }
+  end
+
+  def self.from_hash(hash)
+    self.new(
+      hash['name'],
+      hash['genre_name'],
+      on_spotify: hash['on_spotify'],
+      publish_date: hash['publish_date'],
+      archived: hash['archived']
+    )
   end
 end
